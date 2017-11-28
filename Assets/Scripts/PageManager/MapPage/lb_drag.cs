b46f71769323897a52a3b529cba8bcb551abff23
p_pham <p_pham@yume-sol.com>
2017-11-28 11:09:21 +0700

    Improve Map swiping more smooth 2

--- Assets/Scripts/PageManager/MapPage/lb_drag.cs
+++ Assets/Scripts/PageManager/MapPage/lb_drag.cs
@@ -3,16 +3,14 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 public class lb_drag : MonoBehaviour
 {
-
     private Vector3 Origin;
     private Vector3 Diference;
-    public GameObject[] point;
-    float xxx, yyy;
     private Vector3 velocity = Vector3.zero;
-    bool chayxa;
     Vector3 toadonew;
-    public GameObject[] img_thanh;
 
+    private Vector3 screenPoint;
+    private Vector3 offset;
+    Vector3 curPosition = new Vector3(0, 0, -9);
     void Start()
     {
 
@@ -29,14 +27,11 @@ public class lb_drag : MonoBehaviour
         {
             if (Input.GetMouseButtonDown(0))
             {
-                //Debug.Log(Origin);
                 Origin = MousePos();
             }
             if (Input.GetMouseButtonUp(0))
             {
-                //Debug.Log(MousePos());
 
-                //  Debug.Log(Origin);
             }
             //if (Input.GetMouseButton(0))
             //{
@@ -45,19 +40,17 @@ public class lb_drag : MonoBehaviour
             //    if (Diference.magnitude >= 10)
             //    {
             //        Diference = Diference.normalized;
-
             //        toadonew = this.transform.position + new Vector3(Diference.x, 0, Diference.y) / 1.8f;// * 1.5f
             //        Origin = MousePos();
             //    }
-
             //}
             if (this.transform.position != toadonew)
             {
-                this.transform.position = Vector3.SmoothDamp(this.transform.position, toadonew, ref velocity, 0.10f);//0.10
+               // this.transform.position = Vector3.SmoothDamp(this.transform.position, toadonew, ref velocity, 0.10f);//0.10
 
             }
         }
-
+        this.transform.position = Vector3.SmoothDamp(this.transform.position, curPosition, ref velocity, 0.11f);//0.10}
 
     }
     Vector3 MousePos()
@@ -65,20 +58,19 @@ public class lb_drag : MonoBehaviour
         return Input.mousePosition;
         return Camera.main.WorldToScreenPoint(Input.mousePosition);
     }
+    void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+
+    }
+
     void OnMouseDrag()
     {
-        //if (Input.GetMouseButton(0))
-        //{
-        //    Diference = MousePos() - Origin;
-        //    //Debug.Log(Diference.magnitude);
-        //    if (Diference.magnitude >= 10)
-        //    {
-        //        Diference = Diference.normalized;
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
 
-        //        toadonew = this.transform.position + new Vector3(Diference.x, 0, Diference.y) / 2.5f;// * 1.5f
-        //        Origin = MousePos();
-        //    }
-
-        //}
+        curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        //transform.position = curPosition;
     }
 }
