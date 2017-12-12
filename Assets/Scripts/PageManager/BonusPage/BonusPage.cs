cae4953375c904f3802c1d4239907e17bd330bb6
Tung Vu <vuthanhtung2107@gmail.com>
2017-12-12 09:23:43 +0700

    add tutorial button

--- Assets/Scripts/PageManager/BonusPage/BonusPage.cs
+++ Assets/Scripts/PageManager/BonusPage/BonusPage.cs
@@ -5,84 +5,89 @@ using UnityEngine.UI;
 
 public class BonusPage : Page {
     
-    [SerializeField]
-    GameObject buttonTicket,btShowPhoto,btShowTicket;
+    #region Declare
+        [SerializeField]
+        GameObject buttonTicket,btShowPhoto,btShowTicket;
 
-    Color myGray = new Color();
-    Color myWhite = new Color();
-    List<Image> lstImg = new List<Image>();
+        Color myGray = new Color();
+        Color myWhite = new Color();
+        List<Image> lstImg = new List<Image>();
+    #endregion
 
-    
+    #region Init
+        private void Awake()
+        {
+            ColorUtility.TryParseHtmlString("#9C9C9CFF", out myGray);
+            ColorUtility.TryParseHtmlString("#FFFFFFFF", out myWhite);
+        }
+        void OnEnable()
+        {
 
-    private void Awake()
-    {
-        ColorUtility.TryParseHtmlString("#9C9C9CFF", out myGray);
-        ColorUtility.TryParseHtmlString("#FFFFFFFF", out myWhite);
-    }
+            if (ApplicationLogic.IsShowPhotoFrame())
+                ChangeColor(btShowPhoto, lstImg, true);
+            else
+                ChangeColor(btShowPhoto, lstImg, false);
 
-    void OnEnable ()
-    {
-        
-        if (ApplicationLogic.IsShowPhotoFrame())
-            ChangeColor(btShowPhoto, lstImg, true);
-        else
-            ChangeColor(btShowPhoto, lstImg, false);
-
-        if (ApplicationLogic.IsShowTicket())
-            ChangeColor(btShowTicket, lstImg, true);
-        else
-            ChangeColor(btShowTicket, lstImg, false);
+            if (ApplicationLogic.IsShowTicket())
+                ChangeColor(btShowTicket, lstImg, true);
+            else
+                ChangeColor(btShowTicket, lstImg, false);
 
 
-        if (UserData.IsGotTicket())
-            buttonTicket.SetActive(true);
-        else
+            if (UserData.IsGotTicket())
+                buttonTicket.SetActive(true);
+            else
+                buttonTicket.SetActive(false);
+        }
+    #endregion
+
+    #region Utility
+        void ChangeColor(GameObject bt, List<Image> lstImg, bool Active)
+        {
+            if (lstImg.Count > 0)
+                lstImg.Clear();
+
+            if (Active)
+            {
+                bt.GetComponent<Button>().enabled = true;
+                bt.GetComponentsInChildren<Image>(true, lstImg);
+                foreach (var item in lstImg)
+                {
+                    item.color = myWhite;
+                }
+            }
+            else
+            {
+                bt.GetComponent<Button>().enabled = false;
+                bt.GetComponentsInChildren<Image>(true, lstImg);
+                foreach (var item in lstImg)
+                {
+                    item.color = myGray;
+                }
+            }
+        }
+        public void Click()
+        {
+            UserData.TakeTicket();
             buttonTicket.SetActive(false);
-    }
-
-    void ChangeColor(GameObject bt , List<Image> lstImg , bool Active)
-    {
-        if (lstImg.Count > 0)
-            lstImg.Clear();
-
-        if (Active)
-        {
-            bt.GetComponent<Button>().enabled = true;
-            bt.GetComponentsInChildren<Image>(true, lstImg);
-            foreach (var item in lstImg)
-            {
-                item.color = myWhite;
-            }
         }
-        else
-        {
-            bt.GetComponent<Button>().enabled = false;
-            bt.GetComponentsInChildren<Image>(true, lstImg);
-            foreach (var item in lstImg)
-            {
-                item.color = myGray;
-            }
-        }
-    }
-
-    public void ChangePhotoPage()
-    {
-        PageManager.Show(PageType.PhotoFrame);
-    }
-    public void ChangeMapPage()
-    {
-        PageManager.Show(PageType.MapPage);
-    }
-    // Use this for initialization
-    public void Click ()
-    {
-        UserData.TakeTicket();
-        buttonTicket.SetActive(false);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    #endregion
     
+    #region ChangePage
+        public void ChangePhotoPage()
+        {
+            PageManager.Show(PageType.PhotoFrame);
+        }
+        public void ChangeMapPage()
+        {
+            PageManager.Show(PageType.MapPage);
+        }
+        public void ChangeTutorialPage()
+        {
+            PageManager.Show(PageType.Tutorial);
+            TutorialManager.isBonusPage = true;
+        }
+    #endregion
+
+
 }
