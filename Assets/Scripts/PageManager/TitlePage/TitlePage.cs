c87d30ca02b74130fb5d9496b105a0b2c2def6c9
dung <d_nguyen@yume-sol.com>
2018-01-25 11:28:22 +0700

    Pick language based on language of OS

--- Assets/Scripts/PageManager/TitlePage/TitlePage.cs
+++ Assets/Scripts/PageManager/TitlePage/TitlePage.cs
@@ -29,6 +29,28 @@ public class TitlePage : Page
 
     #region Init
     void OnEnable (){
+		switch (Application.systemLanguage.ToString()) {
+		case "Japanese":
+			SelectJapanese ();
+			ApplicationData.SelectedLanguage = LanguageType.Japanese;
+			break;
+		case "ChineseTraditional":
+			SelectChinese1 ();
+			ApplicationData.SelectedLanguage = LanguageType.Chinese1;
+			break;
+		case "ChineseSimplified":
+			SelectChinese2 ();
+			ApplicationData.SelectedLanguage = LanguageType.Chinese2;
+			break;
+		case "Thai":
+			SelectThai ();
+			ApplicationData.SelectedLanguage = LanguageType.Thai;
+			break;
+		default:
+			SelectEnglish ();
+			ApplicationData.SelectedLanguage = LanguageType.English;
+			break;
+		}
         PageData.Initialize ();
         androidID = "com.fujiwaranosato.EsashiNavi";
         iosID = "youtube-watch-listen-stream/id544007664?mt=8";
