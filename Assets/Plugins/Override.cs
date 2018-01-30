ad8739e9973f8824997cdbe2aad2536e2af0d81c
Duong <ngocduongcntt@gmail.com>
2018-01-30 10:32:38 +0700

    add plugin IOS

---
+++ Assets/Plugins/Override.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Runtime.InteropServices;
+
+public class Override : MonoBehaviour
+{
+    public Text label;
+
+#if !UNITY_EDITOR && UNITY_IOS
+
+  [DllImport("__Internal")]
+  static extern string getMessage();
+
+  void Awake()
+    { 
+        label.text = getMessage(); 
+    }
+#endif
+}
